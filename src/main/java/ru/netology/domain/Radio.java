536d63016fcd5f342d7a92246a5bd76cdd3db247
iva1111 <mvg75@mail.ru>
2021-08-10 14:11:28 +0500

    add correction error

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -4,77 +4,64 @@ public class Radio {
 
     private int minStation = 0;
     private int maxStation = 9;
-    private int currentStation;
-
+    private int station;
     private int minVolume = 0;
     private int maxVolume = 10;
-    private int currentVolume;
+    private int volume;
+
 
     // текущая станция
-    public int getCurrentStation() {
-        return currentStation;
+    public int getStation() {
+        return station;
     }
 
-    // следующая станция
+    public void setStation(int station) {
+        this.station = station;
+    }
+
+    //  следующая станция
     public void nextStation() {
-        if (currentStation <= maxStation) {
-            currentStation = 0;
+        if (station < maxStation) {
+            station++;
+        } else {
+            station = 0;
         }
     }
 
     // предыдущая станция
     public void prevStation() {
-        if (currentStation <= minStation) {
-            currentStation = 9;
+        if (station > 0) {
+            station--;
         } else {
-            this.currentStation = currentStation - 1;
+            station = maxStation;
         }
     }
 
-    // установи текущую станцию
-    public void setCurrentStation(int currentStation) {
-        if (currentStation > maxStation) {
-            return;
-        }
-        if (currentStation < minStation) {
-            return;
-        }
-        this.currentStation = currentStation;
-    }
+
 
     // текущий звук
-    public int getCurrentVolume() {
-        return currentVolume;
+    public int getVolume() {
+        return volume;
     }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
 
     // увеличение звука
     public void plusVolume() {
-        if (currentVolume > maxVolume) {
-            currentVolume = currentVolume + 1;
+        if (volume < maxVolume) {
+            volume++;
         }
     }
 
     // уменьшение звука
     public void minusVolume() {
-        if (currentVolume == minVolume) {
-            return;
+        if (volume > minVolume) {
+            volume--;
         }
-            this.currentVolume = currentVolume - 1;
-        }
-
-
-    // установи текущий звук
-    public void setCurrentVolume(int currentVolume) {
-        if (currentVolume > maxVolume) {
-            return;
-        }
-        if (currentVolume < minVolume) {
-            return;
-        }
-        this.currentVolume = currentVolume;
     }
-
-
 }
 
 
--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -8,112 +8,75 @@ class RadioTest {
 
     Radio radio = new Radio();
 
-    @Test  // установить номер радиостанции 9
-    public void setRadioStationNamber9() {
-        radio.setCurrentStation(9);
-        assertEquals(9, radio.getCurrentStation());
-    }
-
-    @Test  // установить номер радиостанции 8
-    public void setRadioStationNamber8() {
-        radio.setCurrentStation(8);
-        assertEquals(8, radio.getCurrentStation());
-    }
-
-    @Test  // установить номер радиостанции 7
-    public void setRadioStationNamber7() {
-        radio.setCurrentStation(7);
-        assertEquals(7, radio.getCurrentStation());
-    }
-
-    @Test  // установить номер радиостанции 6
-    public void setRadioStationNamber6() {
-        radio.setCurrentStation(6);
-        assertEquals(6, radio.getCurrentStation());
-    }
-
-    @Test  // установить номер радиостанции 5
-    public void setRadioStationNamber5() {
-        radio.setCurrentStation(5);
-        assertEquals(5, radio.getCurrentStation());
-    }
-
-    @Test  // установить номер радиостанции 4
-    public void setRadioStationNamber4() {
-        radio.setCurrentStation(4);
-        assertEquals(4, radio.getCurrentStation());
-    }
-
-    @Test  // установить номер радиостанции 3
-    public void setRadioStationNamber3() {
-        radio.setCurrentStation(3);
-        assertEquals(3, radio.getCurrentStation());
-    }
-
-    @Test  // установить номер радиостанции 2
-    public void setRadioStationNamber2() {
-        radio.setCurrentStation(2);
-        assertEquals(2, radio.getCurrentStation());
-    }
-
-    @Test  // установить номер радиостанции 1
-    public void setRadioStationNamber1() {
-        radio.setCurrentStation(1);
-        assertEquals(1, radio.getCurrentStation());
-    }
-
-    @Test  // установить номер радиостанции 0
-    public void setRadioStationNamber0() {
-        radio.setCurrentStation(0);
-        assertEquals(0, radio.getCurrentStation());
-    }
-
-    @Test  // установи следующую станцию после 9 - 0
-    public void setNextStationAfterNinthZero() {
-        radio.setCurrentStation(9);
+    @Test  // установи станцию после 9 - 0
+    public void setNextStationAfterNine() {
+        radio.setStation(9);
         radio.nextStation();
-        assertEquals(0, radio.getCurrentStation());
+        assertEquals(radio.getStation(), 0);
     }
 
-    @Test  // установи следующую станцию после 0 - 9
-    public void setNextStationAfterZeroNinth() {
-        radio.setCurrentStation(0);
+
+    @Test  // установи станцию перед 0 - 9
+    public void setPrevStationAfterZero() {
+        radio.setStation(0);
         radio.prevStation();
-        assertEquals(9, radio.getCurrentStation());
+        assertEquals(radio.getStation(), 9);
+    }
+
+    @Test  // установить станцию после 0 - 1
+    public void setNextStationAfterZero() {
+        radio.setStation(0);
+        radio.nextStation();
+        assertEquals(radio.getStation(), 1);
+    }
+
+    @Test  // установи станцию перед 9 - 8
+    public void setRadioStationNamberNine() {
+        radio.setStation(9);
+        radio.prevStation();
+        assertEquals(radio.getStation(), 8);
+    }
+
+    @Test // установи любой номер станции
+    public void setAnyStationNamber() {
+        radio.setStation(4);
+        assertEquals(radio.getStation(), 4);
     }
 
 
-
-
-    @Test // установи звук 0
-    public void setVolumeNamber0() {
-        radio.setCurrentVolume(0);
-        assertEquals( 0,radio.getCurrentVolume());
-    }
-
-    @Test // установи звук 10
-    public void setVolumeNamber10() {
-        radio.setCurrentVolume(10);
-        assertEquals( 10,radio.getCurrentVolume());
-    }
-
-
-    @Test // если звук max то ноль
-    public void volumeMaxThenZero() {
-        radio.setCurrentVolume(10);
-        radio.plusVolume();
-        assertEquals(10, radio.getCurrentVolume());
-    }
-
-    @Test // если звук min то ноль
-    public void volumeMinThenZero() {
-        radio.setCurrentVolume(0);
+    @Test // установи звук меньше 0
+    public void setVolumeLess0() {
+        radio.setVolume(0);
         radio.minusVolume();
-        assertEquals(0, radio.getCurrentVolume());
+        assertEquals(radio.getVolume(), 0);
+    }
+
+    @Test // установи звук больше 10
+    public void setVolumeMore10() {
+        radio.setVolume(10);
+        radio.plusVolume();
+        assertEquals(radio.getVolume(), 10);
+    }
+
+
+    @Test // увеличить звук свыше 0
+    public void plusVolumeOverMax() {
+        radio.setVolume(0);
+        radio.plusVolume();
+        assertEquals(radio.getVolume(), 1);
+    }
+
+    @Test // уменьшить звук меньше 10
+    public void minusVolumeMinThenZero() {
+        radio.setVolume(10);
+        radio.minusVolume();
+        assertEquals(radio.getVolume(), 9);
     }
 
 
 }
+
+
 
 
 
